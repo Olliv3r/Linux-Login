0d68278ff1e6b5651761e5a5588239a59b16cca4
Olliv3r <oliveobom100@gmail.com>
2022-12-25 16:00:10 -0400

    Refatorado

--- login.sh
+++ login.sh
@@ -1,154 +1,211 @@
 #!/usr/bin/env bash
 # login.sh
 #
-# 
+# Oliver, Setembro de 2010 
 
+# Dados da entrada
 unset usuario
 unset senha
+unset caractere
 
-# Chaves de ativação das funcionalidades
+# Chaves
 chave_usuario=0
 chave_senha=0
 
-# Variáveis login padrão (Pode alterar)
-username="root"
-password="toor"
+# Login padrão (Pode alterar)
+login_usuario="root"
+login_senha="toor"
 
-# Mascara ou usuário ov senha
+# Mascara
 caractere=''
 
-message_interrupt() {
+# Cores
+w="\e[0m"
+r="\e[31m"
+g="\e[32m"
+
+# Ctr+c, Ctr+z e etc
+trap aviso_teclas_proibidas SIGINT SIGTSTP
+
+# Pacotes necessários
+pacotes=(
+	"neofetch" 
+	"tput"
+)
+
+# Instalador de pacotes
+instalador() {
+    if [ ${1} == ${pacotes[1]} -a ! -f ${PREFIX}/bin/tput ] || [ ${1} != ${pacotes[1]} -a ! -f ${PREFIX}/bin/${1} ] ; then
+        printf "\r[*] Instalando pacote ${1}..."
+        apt update > /dev/null 2>&1
+        apt install $1 -y > /dev/null 2>&1
+
+	if [ ${1} == ${pacotes[1]} -a -f ${PREFIX}/bin/tput ] || [ ${1} != ${pacotes[1]} -a -f ${PREFIX}/bin/${1} ] ; then
+	    printf "\r[*] Instalando pacote ${1}...OK\n"
+	    sleep 0.5
+	fi
+    fi
+}
+
+# Pacote inexistente
+aviso_pacote_inexistente() {
+    for ((index = 0; index < ${#pacotes[*]}; index++))
+    do
+	if [ ${pacotes[index]} == ${pacotes[1]} ] ; then
+	    pacotes[index]="ncurses-utils"
+	fi
+
+	instalador ${pacotes[index]}
+    done
+}
+
+# Teclas proibidas
+aviso_teclas_proibidas() {
 	unset usuario
 	unset senha
+	unset caractere
+	
 	tput setaf 1
 	echo -e "\e[1;5;40m=> \e[0;5;41mAtalho proíbido!\e[0m"
 	tput sgr0
-	echo -ne "\e[0;5;44m$texto_adm:\e[0m "
+	printf "\e[0;5;44m${texto_usuario_ou_senha}:\e[0m "
 }
 
-texto_adm="Usuário"
-trap message_interrupt SIGINT SIGTSTP
+# Banner
+banner() {
+   tput reset
+   neofetch --ascii_distro kali_small -L
+   echo
+   echo -n "Linux Login *"
+   echo
+   echo
+}
 
-echo -e "Painel adm Termux ***\n"
+# Usuário
+f_usuario() {
 
-printf "\r\e[0;5;44mUsuário:\e[0m "
+    texto_usuario_ou_senha="Usuário"
+    texto_usuario="Usuário"
 
-while IFS= read -r -s -n1 mascara_usuario; do
-	texto_adm="Usuário"
+    printf "\r\e[0;5;44m${texto_usuario}:\e[0m "
 
-	if [[ -z $mascara_usuario ]] ; then
-		tput setaf 1
-		echo -e "\e[1;5;40m => \e[0;5;41mUsuário inválido!\e[0m"
-		tput sgr0
-		printf "\r\e[0;5;44m$texto_adm:\e[0m "
+    while IFS= read -r -s -n1 mascara_usuario; do
 
-		unset usuario
-		unset caractere
+	if [[ -z ${mascara_usuario} ]] ; then
+	    tput setaf 1
+   	    echo -e "\e[1;5;40m => \e[0;5;41mUsuário inválido!\e[0m"
+
+	    tput sgr0
+   	    printf "\r\e[0;5;44m${texto_usuario}:\e[0m "
+
+	    unset usuario
+	    unset caractere
 
 	# Backspace (Remove o último caractere)
-	elif [[ $mascara_usuario == $'\x7f' ]] ; then
+	elif [[ ${mascara_usuario} == $'\x7f' ]] ; then
+	    if [[ -n ${caractere} ]] ; then
+		caractere=${caractere%?}
+	    fi
 
-		if [[ -n $caractere ]] ; then
-			caractere=${caractere%?}
-		fi
-
-		if [[ -n $usuario ]] ; then
-			usuario=${usuario%?}
-			printf "\b \b"
-		fi
+	    if [[ -n ${usuario} ]] ; then
+		usuario=${usuario%?}
+		printf "\b \b"
+	    fi
 	
 	# Bloqueia teclas ESC, TAB e ESPAÇO
-	elif [[ $mascara_usuario == $'\x1B' ]] || 
-	     [[ $mascara_usuario == $'\x09' ]] ||
-	     [[ $mascara_usuario == $'\x20' ]] ; then
-		tput setaf 1
-		echo -e "\e[1;5;40m => \e[0;5;41mTacla proíbida!\e[0m"
-		tput sgr0
-		printf "\r\e[0;5;44m$texto_adm:\e[0m "
-		unset usuario
-		unset caractere
+	elif [[ ${mascara_usuario} == $'\x1B' ]] || 
+	     [[ ${mascara_usuario} == $'\x09' ]] ||
+	     [[ ${mascara_usuario} == $'\x20' ]] ; then
+	    tput setaf 1
+	    echo -e "\e[1;5;40m => \e[0;5;41mTacla proíbida!\e[0m"
+	    tput sgr0
+	    printf "\r\e[0;5;44m${texto_usuario}:\e[0m "
+
+	    unset usuario
+	    unset caractere
 
 	else
-		usuario+=$mascara_usuario
-		caractere+=$mascara_usuario
+	    usuario+=${mascara_usuario}
+	    caractere+=${mascara_usuario}
 
-		# Mascara ou caractere (pode alterar)
-		echo -ne "\e[32;5m$caractere\e[0m"
+	    # Mascara ou caractere (pode alterar)
+	    echo -ne "\e[32;5m${caractere}\e[0m"
 
-		if [[ $usuario == $username ]] ; then
-			chave_usuario=1
-			break
-		fi
+	    if [[ ${usuario} == ${login_usuario} ]] ; then
+		chave_usuario=1
+		break
+	    fi
 
-		unset caractere
+	    unset caractere
 	fi
-done
-
-echo
-
-texto_adm="Senha"
-unset caractere
-trap message_interrupt SIGINT SIGTSTP
-
-printf "\r\e[0;5;44m$texto_adm:\e[0m "
-
-while IFS= read -r -s -n1 mascara_senha; do
-	texto_adm="Senha"
-
-	if [[ -z $mascara_senha ]] ; then
-		tput setaf 1
-		echo -e "\e[1;5;40m => \e[0;5;41mSenha inválida!\e[0m"
-		tput sgr0
-		printf "\r\e[0;5;44m$texto_adm:\e[0m "
-		unset senha
-		unset caractere
-
-	# Backspace (Remove o último caractere)
-	elif [[ $mascara_senha == $'\x7f' ]] ; then
-
-		if [[ -n $caractere ]] ; then
-			caractere=${caractere%?}
-		fi
-
-		if [[ -n $senha ]] ; then
-			senha=${senha%?}
-			printf "\b \b"
-		fi
-
-	# Bloqueia teclas ESC, TAB e ESPAÇO
-	elif [[ $mascara_senha == $'\x1B' ]] || 
-	     [[ $mascara_senha == $'\x09' ]] ||
-	     [[ $mascara_senha == $'\x20' ]] ; then
-		tput setaf 1
-		echo -e "\e[1;5;40m => \e[0;5;41mTacla proíbida!\e[0m"
-		tput sgr0
-		printf "\r\e[0;5;44m$texto_adm:\e[0m "
-		unset senha
-		unset caractere
-
-	else
-		senha+=$mascara_senha
-		caractere+=$mascara_senha
-
-		# Mascara ou caractere (pode alterar)
-		printf "\e[32;5m×\e[0m"
-
-		if [[ $senha == $password ]] ; then
-			chave_senha=1
-			break
-		fi
-
-		unset caractere
-	fi
-done
-
-acesso() {
-    tput reset
+    done
     echo
-    echo
-    neofetch --ascii_distro kali_small -L
 }
 
-# Execução
+f_senha() {
+    texto_usuario_ou_senha="Senha" 
+    texto_senha="Senha"
 
-[ $chave_usuario == 1 ] && [ $chave_senha == 1 ] && acesso && exit 0
+    printf "\r\e[0;5;44m${texto_senha}:\e[0m "
+
+    while IFS= read -r -s -n1 mascara_senha; do
+
+	if [[ -z ${mascara_senha} ]] ; then
+	    tput setaf 1
+	    echo -e "\e[1;5;40m => \e[0;5;41mSenha inválida!\e[0m"
+	    
+	    tput sgr0
+	    printf "\r\e[0;5;44m${texto_senha}:\e[0m "
+	
+	    unset senha
+	    unset caractere
+
+	# Backspace (Remove o último caractere)
+	elif [[ ${mascara_senha} == $'\x7f' ]] ; then
+	    if [[ -n ${caractere} ]] ; then
+		caractere=${caractere%?}
+	    fi
+
+	    if [[ -n ${senha} ]] ; then
+		senha=${senha%?}
+		printf "\b \b"
+	    fi
+
+	# Bloqueia teclas ESC, TAB e ESPAÇO
+	elif [[ ${mascara_senha} == $'\x1B' ]] || 
+	     [[ ${mascara_senha} == $'\x09' ]] ||
+	     [[ ${mascara_senha} == $'\x20' ]] ; then
+	    tput setaf 1
+	    echo -e "\e[1;5;40m => \e[0;5;41mTacla proíbida!\e[0m"
+		
+	    tput sgr0
+	    printf "\r\e[0;5;44m${texto_senha}:\e[0m "
+
+	    unset senha
+	    unset caractere
+
+	else
+	    senha+=${mascara_senha}
+	    caractere+=${mascara_senha}
+
+	    # Mascara ou caractere (pode alterar)
+	    printf "\e[32;5m×\e[0m"
+
+	    if [[ ${senha} == ${login_senha} ]] ; then
+		chave_senha=1
+		break
+	    fi
+
+	    unset caractere
+	fi
+    done
+}
+
+aviso_pacote_inexistente
+banner
+f_usuario
+f_senha
+
+[ ${chave_usuario} == 1 ] && 
+[ ${chave_senha} == 1 ] && 
+   echo -e "\n\nNew window\n"

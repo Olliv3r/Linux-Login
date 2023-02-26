949b0ae4744230b66fddc0eb817612d112207c73
Olliv3r <oliveobom100@gmail.com>
2023-02-26 12:23:25 -0400

    ∗

--- login.sh
+++ login.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# 
-# 
+#
+#
 
 # Variáveis resetadas
 unset username
@@ -13,7 +13,7 @@ unset key_password
 # Acesso padrão
 username_default="root" # Pode alterar
 password_default="toor" # Pode alterar
-mask='•'		# Pode alterar
+mask='∗'		# Pode alterar
 caracter=''
 
 # Textos de uso das funçôes
@@ -48,7 +48,7 @@ alert_password() {
 # Entrada de usuário
 inputUser() {
   trap alert_user SIGINT SIGTSTP
-  
+
   printf "\r${msg[0]}: "
   while IFS= read -r -s -n1 input_username ; do
     if [ -z "$input_username" ] ; then
@@ -62,32 +62,32 @@ inputUser() {
       if [ -n "$caracter" ] ; then
   	    caracter=${caracter%?}
       fi
-  
+
       if [ -n "$username" ] ; then
       	username=${username%?}
       	printf "\b \b"
       fi
-      
+
     elif [ "$input_username" == $'\x1B' ] ||
     [ "$input_username" == $'\x09' ] ||
     [ "$input_username" == $'\x20' ] ; then
       printf "\r${msg[4]}!\n"
       printf "\r${msg[0]}: "
-      
+
       unset username
       unset caracter
-      
+
   	else
       username+=$input_username
       caracter+=$input_username
-  
+
       echo -ne "$caracter"
-  
+
       if [ "$username" == "$username_default" ] ; then
         key_user=$username
         break
       fi
-  
+
   	  unset caracter
   	fi
   done
@@ -95,47 +95,47 @@ inputUser() {
 # Entrada de senha
 inputPassword() {
   trap alert_password SIGINT SIGTSTP
-  
+
   printf "\n"
   printf "\r${msg[1]}: "
   while IFS= read -r -s -n1 input_password ; do
     if [ -z "$input_password" ] ; then
       printf "\r${msg[3]}\n"
       printf "\r${msg[1]}: "
-      
+
       unset password
       unset caracter
-      
+
     elif [ "$input_password" == $'\x7f' ] ; then
       if [ -n "$caracter" ] ; then
         caracter=${caracter%?}
       fi
-      
+
       if [ -n "$password" ] ; then
         password=${password%?}
         printf "\b \b"
       fi
-      
+
     elif [ "$input_password" == $'\x1B' ] ||
     [ "$input_password" == $'\x09' ] ||
     [ "$input_password" == $'\x20' ] ; then
       printf "\r${msg[4]}!\n"
       printf "\r${msg[1]}: "
-      
+
       unset password
       unset caracter
-      
+
     else
       password+=$input_password
       caracter+=$input_password
-      
+
       echo -ne "$mask"
-      
+
       if [ "$password" == "$password_default" ] ; then
         key_password=$password
         break
       fi
-      
+
       unset caracter
     fi
   done

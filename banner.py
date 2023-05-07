89bc244dd3ba650cc287a06d3d1dc4fbe2db835d
Olliv3r <oliveobom100@gmail.com>
2023-05-06 22:50:33 -0400

    $

---
+++ banner.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# Logos aleatórios
+#
+# Por oliver
+#
+
+import random
+import os
+
+root = "/data/data/com.termux/files/home/Linux-Login"
+
+def banner():
+
+    regex = 'neofetch --help | tr -d "\n " | grep -Eo "AIX.*Zorin" | sed -e "s/,/\\n/g" | tr -d \\"'
+    distros = os.system(regex + str(f" > {root}/distros.txt"))
+
+    distros = open(f"{root}/distros.txt", 'r')
+
+    distro = random.choice(list(distros)).rstrip()
+
+    os.system(f'neofetch --ascii_distro {distro} -L')
+    
+def _input():
+
+    if not os.path.exists(f"{root}/.banner"):
+        res = input('Apply random logo? y/n\n')
+
+        if res == "y" or res == "yes":
+            os.system(f"echo 'on' > {root}/.banner")
+    
+        elif res == "n" or res == "no":
+            os.system(f"echo 'off' > {root}/.banner")
+
+        else:
+            _input()
+
+    else:
+        
+        banner()
+
+
+_input()
